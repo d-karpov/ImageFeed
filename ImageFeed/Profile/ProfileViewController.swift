4c8c618553e633a1049d9d4c1b8a98a2f01c742d
d-karpov <77918194+d-karpov@users.noreply.github.com>
2024-07-10 17:52:22 +0300

    Экран Profile сверстан

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Denis on 10.07.2024.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
