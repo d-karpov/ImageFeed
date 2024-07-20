611b8154c45419ad9b7cbd39fd35cb368e3452ae
d-karpov <77918194+d-karpov@users.noreply.github.com>
2024-07-20 18:11:09 +0300

    Учтены коменты по ревью

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -8,13 +8,48 @@
 import UIKit
 
 
-class ProfileViewController: UIViewController {
+final class ProfileViewController: UIViewController {
 	//MARK: - Private variables
-	private lazy var profileImage: UIImageView = makeProfileImage()
-	private lazy var exitButton: UIButton = makeExitButton()
-	private lazy var nameLabel: UILabel = makeFullNameLabel()
-	private lazy var nickLabel: UILabel = makeNickLabel()
-	private lazy var infoLabel: UILabel = makeInfoLabel()
+	private lazy var profileImage: UIImageView = {
+		let imageView = UIImageView()
+		imageView.translatesAutoresizingMaskIntoConstraints = false
+		if let image = UIImage(named: "Mock User") {
+			imageView.image = image
+		} else {
+			imageView.tintColor = .ypGray
+			imageView.image = UIImage(systemName: "person.crop.circle.fill")
+		}
+		imageView.layer.cornerRadius = Sizes.ProfileImage.cornerRadius
+		return imageView
+	}()
+	
+	private lazy var exitButton: UIButton = {
+		let button = UIButton()
+		button.translatesAutoresizingMaskIntoConstraints = false
+		button.setImage(UIImage(named: "Exit"), for: .normal)
+		return button
+	}()
+	
+	private lazy var nameLabel: UILabel = {
+		let label = makeLabel(with: "Екатерина Новикова")
+		label.font = Fonts.bold23
+		label.textColor = .ypWhite
+		return label
+	}()
+	
+	private lazy var nickLabel: UILabel = {
+		let label = makeLabel(with: "@ekaterina_nov")
+		label.font = Fonts.regular13
+		label.textColor = .ypGray
+		return label
+	}()
+	
+	private lazy var infoLabel: UILabel = {
+		let label = makeLabel(with: "Hello World!")
+		label.font = Fonts.regular13
+		label.textColor = .ypWhite
+		return label
+	}()
 	
 	//MARK: - Lifecycle
 	override func viewDidLoad() {
@@ -96,51 +131,10 @@ class ProfileViewController: UIViewController {
 		)
 	}
 	
-	private func makeProfileImage() -> UIImageView {
-		let imageView = UIImageView()
-		imageView.translatesAutoresizingMaskIntoConstraints = false
-		if let image = UIImage(named: "Mock User") {
-			imageView.image = image
-		} else {
-			imageView.tintColor = .ypGray
-			imageView.image = UIImage(systemName: "person.crop.circle.fill")
-		}
-		imageView.layer.cornerRadius = Sizes.ProfileImage.cornerRadius
-		return imageView
-	}
-	
-	private func makeExitButton() -> UIButton {
-		let button = UIButton()
-		button.translatesAutoresizingMaskIntoConstraints = false
-		button.setImage(UIImage(named: "Exit"), for: .normal)
-		return button
-	}
-	
 	private func makeLabel(with text: String) -> UILabel {
 		let label = UILabel()
 		label.translatesAutoresizingMaskIntoConstraints = false
 		label.text = text
-		return label
-	}
-	
-	private func makeFullNameLabel() -> UILabel {
-		let label = makeLabel(with: "Екатерина Новикова")
-		label.font = Fonts.bold23
-		label.textColor = .ypWhite
-		return label
-	}
-	
-	private func makeNickLabel() -> UILabel {
-		let label = makeLabel(with: "@ekaterina_nov")
-		label.font = Fonts.regular13
-		label.textColor = .ypGray
-		return label
-	}
-	
-	private func makeInfoLabel() -> UILabel {
-		let label = makeLabel(with: "Hello World!")
-		label.font = Fonts.regular13
-		label.textColor = .ypWhite
 		return label
 	}
 }
