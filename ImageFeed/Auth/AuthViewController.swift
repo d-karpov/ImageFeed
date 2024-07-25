b163ef2681ab1f07dcf4c65818502e4b4b6d08cb
d-karpov <77918194+d-karpov@users.noreply.github.com>
2024-07-25 23:11:41 +0300

    Добавлен экран входа, подготовлен к навигации

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Denis on 25.07.2024.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+
+}
---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Denis on 25.07.2024.
+//
+
+import Foundation
+
+enum Constants {
+	static let accessKey: String = "zD94Nskkw-ORsKnrZEhfJpxDNDsQfOZgvB24DjNFAMw"
+	static let secretKey: String = "xL9uaqAgQAGS9QliU8KfuMAqPuO4LR-PYZPAo6p_R4w"
+	static let redirectURI: String = "urn:ietf:wg:oauth:2.0:oob"
+	static let accessScope: String = "public"
+	static let defaultBaseURL: URL = .init(string: "https://api.unsplash.com/")!
+}
