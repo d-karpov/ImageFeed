d30bc849a5588fee3120acf53b9e3a42beea7298
d-karpov <77918194+d-karpov@users.noreply.github.com>
2024-07-26 14:32:12 +0300

    Добавлен ProgressView и WebView

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -7,10 +7,49 @@
 
 import UIKit
 
-class AuthViewController: UIViewController {
+final class AuthViewController: UIViewController {
+	
+	private let webViewSegueIdentifier = "ShowWebView"
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		configureBackButton()
+	}
+	
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		if 
+			segue.identifier == webViewSegueIdentifier,
+			let webViewViewController = segue.destination as? WebViewViewController {
+			webViewViewController.delegate = self
+		} else {
+			super.prepare(for: segue, sender: sender)
+		}
+	}
+	
+	private func configureBackButton() {
+		navigationController?.navigationBar.backIndicatorImage = .navBackButton
+		navigationController?.navigationBar.backIndicatorTransitionMaskImage = .navBackButton
+		navigationItem.backBarButtonItem = UIBarButtonItem(
+			title: "",
+			style: .plain,
+			target: .none,
+			action: #selector(webViewViewControllerDidCancel)
+		)
+		navigationItem.backBarButtonItem?.tintColor = .ypBlack
 	}
 
+}
+
+extension AuthViewController: WebViewViewControllerDelegate {
+	
+	func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+		
+	}
+	
+	@objc
+	func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
+		dismiss(animated: true)
+	}
+	
+	
 }
--- ImageFeed/Constants.swift
+++ ImageFeed/Constants.swift
@@ -8,9 +8,13 @@
 import Foundation
 
 enum Constants {
-	static let accessKey: String = "zD94Nskkw-ORsKnrZEhfJpxDNDsQfOZgvB24DjNFAMw"
-	static let secretKey: String = "xL9uaqAgQAGS9QliU8KfuMAqPuO4LR-PYZPAo6p_R4w"
-	static let redirectURI: String = "urn:ietf:wg:oauth:2.0:oob"
+	static let accessKey: String = ""
+	static let secretKey: String = ""
+	static let redirectURI: String = ""
 	static let accessScope: String = "public"
 	static let defaultBaseURL: URL = .init(string: "https://api.unsplash.com/")!
+}
+
+enum WebViewConstants {
+	static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
 }
