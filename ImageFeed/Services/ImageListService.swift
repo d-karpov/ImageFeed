16c0d4f51838721db4e6dcd1707d930319b6a5b9
d-karpov <77918194+d-karpov@users.noreply.github.com>
2024-09-16 19:06:21 +0300

    Исправлено критическое замечание

--- ImageFeed/Services/ImageListService.swift
+++ ImageFeed/Services/ImageListService.swift
@@ -21,36 +21,37 @@ final class ImageListService {
 	static let didChangeNotification: Notification.Name = .init("ImageListServiceDidChange")
 	
 	private let requestBuilder: RequestsBuilderService = .shared
+	private let dataFormatter = ISO8601DateFormatter()
 	private (set) var photos: [Photo] = []
 	private var currentPage = 0
 	private var task: URLSessionTask?
 	
 	func fetchPhotosNextPage() {
-		if task == nil {
-			guard let request = requestBuilder.madeRequest(for: .photos(currentPage+1)) else {
-				print(ImageListServiceError.invalidRequest.localizedDescription)
-				return
-			}
-			
-			let task = URLSession.shared
-				.objectTask(for: request) { [weak self] (result: Result<[PhotosResponseBody], Error>) in
-					switch result {
-					case .success(let response):
-						response.forEach { photoResponseBody in
-							if let photo = self?.makePhotoFromResponse(from: photoResponseBody) {
-								self?.photos.append(photo)
-							}
-						}
-						NotificationCenter.default.post(name: Self.didChangeNotification, object: nil)
-						self?.currentPage += 1
-						self?.task = nil
-					case .failure(let error):
-						print("[\(#fileID)]:[\(#function)] -> " + error.localizedDescription)
-					}
-				}
-			self.task = task
-			task.resume()
+		guard task == nil else { return }
+		guard let request = requestBuilder.madeRequest(for: .photos(currentPage+1)) else {
+			print(ImageListServiceError.invalidRequest.localizedDescription)
+			return
 		}
+		
+		let task = URLSession.shared
+			.objectTask(for: request) { [weak self] (result: Result<[PhotosResponseBody], Error>) in
+				switch result {
+				case .success(let response):
+					response.forEach { photoResponseBody in
+						if let photo = self?.makePhotoFromResponse(from: photoResponseBody) {
+							self?.photos.append(photo)
+						}
+					}
+					NotificationCenter.default.post(name: Self.didChangeNotification, object: nil)
+					self?.currentPage += 1
+					self?.task = nil
+				case .failure(let error):
+					self?.task = nil
+					print("[\(#fileID)]:[\(#function)] -> " + error.localizedDescription)
+				}
+			}
+		self.task = task
+		task.resume()
 	}
 	
 	func changeLike(photoId: String, isLike: Bool, _ completion: @escaping (Result<Photo, Error>) -> Void) {
@@ -71,12 +72,12 @@ final class ImageListService {
 						completion(.failure(error))
 					}
 				}
-				task.resume()
+			task.resume()
 		}
 	}
 	
 	private func makePhotoFromResponse(from photoResponseBody: PhotosResponseBody) -> Photo {
-		let createdAt = ISO8601DateFormatter().date(from: photoResponseBody.createdAt)
+		let createdAt = dataFormatter.date(from: photoResponseBody.createdAt)
 		let photo = Photo(
 			id: photoResponseBody.id,
 			size: .init(width: photoResponseBody.width, height: photoResponseBody.height),
